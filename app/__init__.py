b8921e919c6db4e9c111447da5ecf747b018db89
parthsh77 <parth.12214873@lpu.in>
2026-04-19 23:46:17 +0530

    Fix app factory and routing

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,11 @@ from config import Config
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder="../templates", static_folder="../static")
+    app = Flask(
+        __name__,
+        template_folder="../templates",
+        static_folder="../static"
+    )
     app.config.from_object(Config)
     db.init_app(app)
 
@@ -14,4 +18,5 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
     return app
